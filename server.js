7bc1579eb7a8b3c05faf54fa3490ed7ccaade1e5
Daniel <leinad6ix.z@gmail.com>
2022-11-03 18:50:20 -0700

    Created server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
