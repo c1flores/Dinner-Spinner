aa11900177cc31b7014dc2f46256da90f32cb400
Daniel <leinad6ix.z@gmail.com>
2022-11-04 04:56:29 -0700

    Created register/signup javascript

---
+++ public/assets/js/register.js
@@ -0,0 +1,25 @@
+const registerFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
