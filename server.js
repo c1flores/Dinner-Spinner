59c373006633a4e24cdd44acf717966dc3e6cd41
Daniel <leinad6ix.z@gmail.com>
2022-11-03 19:42:17 -0700

    added more functionality to server.js file

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
-const sequelize = require('./config/connection');
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
-const routes = require('./routes');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: 'placeholder',
+  resave: false,
+  saveUnitialized: false,
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes);
 
