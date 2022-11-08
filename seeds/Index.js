c8bafa79b7da78cb394ed36ab59288c4d4ced169
Christian Flores <c1flores@ucsd.edu>
2022-11-08 15:10:21 -0800

    database construction & seed patch

--- seeds/Index.js
+++ seeds/Index.js
@@ -1,13 +1,22 @@
 const sequelize = require('../config/connection');
-const seedRecipe = require('./recipeData');
-const seedUser = require('./userData');
+const Recipe  = require('../models/Recipe');
+const User  = require('../models/User');
+const seedRecipe = require('./recipe-seeds.json');
+const seedUser = require('./user-seeds.json');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedRecipe();
+  await Recipe.bulkCreate(seedRecipe, {
+    individualHooks: true,
+    returning: true,
 
-  await seedUser();
+  });
+
+  await User.bulkCreate(seedUser, {
+    individualHooks: true,
+    returning: true,
+  });
 
   process.exit(0);
 };
