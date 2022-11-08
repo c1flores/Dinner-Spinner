71eb917e0c8e4011b58c9a375f6d87c06a299225
Christian Flores <c1flores@ucsd.edu>
2022-11-07 20:19:45 -0800

    Finish handlebar functionality

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,24 +1,22 @@
-const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in.');
-      }
-    }
-  };
+const loginFormHandler = async function (event) {
+  event.preventDefault();
 
-  document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  const usernameEl = document.querySelector("#username-input-login");
+  const passwordEl = document.querySelector("#password-input-login");
+  fetch("/api/user/login", {
+      method: "post",
+      body: JSON.stringify({
+          username: usernameEl.value,
+          password: passwordEl.value
+      }),
+      headers: { "Content-Type": "application/json" }
+  })
+      .then(function () {
+          document.location.replace("/dashboard");
+      })
+      .catch(err => console.log(err));
+};
+
+document
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
