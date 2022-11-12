3327cc63bda00036eb914cca9e83ae58219bb4e0
whl <wplovitz@gmail.com>
2022-11-12 13:48:31 -0800

    got the profile addrecipe section to communicat with the seed

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,45 +1,45 @@
-// const newFormHandler = async (event) => {
-//     event.preventDefault();
+const newFormHandler = async (event) => {
+    event.preventDefault();
 
-    // const name = document.querySelector('#recipe-name').value.trim();
-    // const instruction = document.querySelector('#recipe-instr').value.trim();
+    const recipeName = document.querySelector('#recipe-name').value.trim();
+    const instruction = document.querySelector('#recipe-instr').value.trim();
 
-    // if (name && instruction) {
-    //     const response = await fetch('/api/recipe', {
-    //         method: 'POST',
-    //         body: JSON.stringify({ name, instruction}),
-    //         headers: {'content-Type': 'application/json'},
-    //     });
+    if (recipeName && instruction) {
+        const response = await fetch('/api/recipes', {
+            method: 'POST',
+            body: JSON.stringify({ recipeName, instruction}),
+            headers: {'content-Type': 'application/json'},
+        });
 
-    //     if (response.ok) {
-    //         document.location.replace('/profile');
-    //     } else {
-    //         alert('Failes to create recipe');
-    //     }
-    // }
-// };
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failes to create recipe');
+        }
+    }
+};
 
-// const delButtonHandler = async (event) => {
-//     if (event.target.hasAttribute('data-id')) {
-//         const id = event.target.getAttribute('data-id');
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
 
-//         const response = await fetch(`/api/recipes/${id}`, {
-//             method: 'DELETE',
-//         });
+        const response = await fetch(`/api/recipes/${id}`, {
+            method: 'DELETE',
+        });
 
-//         if (response.ok) {
-//             document.location.replace('/profile');
-//         }else {
-//             alert('Failed to delete recipe')
-//         }
-//     }
-// };
+        if (response.ok) {
+            document.location.replace('/profile');
+        }else {
+            alert('Failed to delete recipe')
+        }
+    }
+};
 
 
-// document
-//     .querySelector('.new-recipe-form')
-//     .addEventListener('submit', newFormHandler);
+document
+    .querySelector('.new-recipe-form')
+    .addEventListener('submit', newFormHandler);
 
-// document
-//     .querySelector('.recipe-list')
-//     .addEventListener('click', delButtonHandler);
+document
+    .querySelector('.recipe-list')
+    .addEventListener('click', delButtonHandler);
